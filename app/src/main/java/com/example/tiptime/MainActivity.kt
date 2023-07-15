b5ba92878b18f41e9d6797438dc4d668780cfb11
Bernadette <bernadettedejesus99@gmail.com>
2023-07-15 18:04:57 +0800

    first commit

---
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.tiptime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    private fun calculateTip() {
+        // Get the decimal value from the cost of service text field
+    }
+}
